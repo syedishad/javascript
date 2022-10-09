b372975054f5b7cc5cd233c3a910810eb14bd591
syedishad <irshad.syed009@gmail.com>
2022-10-10 01:33:16 +0530

    js

---
+++ sandbox.js
@@ -0,0 +1,52 @@
+// alert('hello World');
+
+
+// console.log(1);
+// console.log('Syed Ishad hussain');
+// console.log(2);
+//  How to declare varaibles
+// let age = 25;
+// let year = 2001;
+// to console in one line
+// console.log(age, year);
+// to console in separate line
+// console.log(year);
+
+// to upadte a varible
+// age = 30;
+// console.log(age);
+// if we dont dont to upadte any varaible then use const keyword
+// we can use Var keyword which is the older version of js
+
+
+
+
+
+
+// strings
+// console.log('hello, world');
+
+// let email = 'mario@thenetninja.co.uk';
+// console.log(email);
+
+// string concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
+
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+// getting individual characters
+// console.log(fullName[2]);
+
+// string length
+// console.log(fullName.length);
+
+// string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLocaleLowerCase();
+// console.log(result);
+
+// let index = email.indexOf('@');
+// console.log('index of the @ sign:', index);
