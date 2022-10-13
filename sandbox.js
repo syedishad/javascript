106e641dc05cc1752a9f3f5d192c435ec8445874
SYED ISHAD HUSSAIN <82176036+syedishad@users.noreply.github.com>
2022-10-14 01:23:08 +0530

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,218 +1,96 @@
-// alert('hello World');
-
-
-// console.log(1);
-// console.log('Syed Ishad hussain');
-// console.log(2);
-//  How to declare varaibles
-// let age = 25;
-// let year = 2001;
-// to console in one line
-// console.log(age, year);
-// to console in separate line
-// console.log(year);
-
-// to upadte a varible
-// age = 30;
-// console.log(age);
-// if we dont dont to upadte any varaible then use const keyword
-// we can use Var keyword which is the older version of js
-
-
-
-
-
-
-// strings
-// console.log('hello, world');
-
-// let email = 'mario@thenetninja.co.uk';
-// console.log(email);
-
-// string concatenation
-// let firstName = 'Brandon';
-// let lastName = 'Sanderson';
-
-// let fullName = firstName + ' ' + lastName;
-
-// console.log(fullName);
-
-// getting individual characters
-// console.log(fullName[2]);
-
-// string length
-// console.log(fullName.length);
-
-// string methods
-// console.log(fullName.toUpperCase());
-// let result = fullName.toLocaleLowerCase();
-// console.log(result);
-
-// let index = email.indexOf('@');
-// console.log('index of the @ sign:', index);
-
-
-// day 2
-// common string methods
-
-let email = 'mario@thenetninja.co.uk';
-
-// let result = email.lastIndexOf('n');
-
-//let result = email.slice(0, 5); (from 0 to5)
-
-//let result = email.substr(5, 12);
-
-//let result = email.replace('m', 'w');
-
-// let result = email.replace('n', 'w');
-
-// console.log(result);
-
-
-
-// numbers
-
-let radius = 10;
-let pi = 3.14;
-
-// console.log(radius, pi);
-
-// math operators - +, -, *, /, **, %
-
-// console.log(10 / 2);
-// let result = radius % 3;
-// let result = pi * radius**2;
-
-// order of operation - B I D M A S (bracket,integer,devision,mutli,add,sub)
-
-// let result = 5 * (10 - 3)**2;
-
-// console.log(result);
-
-// shorthands
-// let likes = 10;
-
-//to add
-
-// likes = likes + 1;
-
-//easy methodd
-
-// likes++;
-
-// likes = likes + 10;
-// likes += 10;
-
-// likes *= 2;
-// likes /= 2;
-
-// console.log(likes);
-
-// NaN - not a number
-
-// console.log(5 / 'hello');
-// console.log(5 * 'hello');
-
-// let result = 'the blog has ' + likes + ' likes';
-// console.log(result);
-
-// template strings
-// const title = 'Best reads of 2019';
-// const author = 'Mario';
-// const likes = 30;
-
-// concatenation way
-//gigantic string(conmbinaton of lettter numbers)
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-// console.log(result);
-
-// template string way
-//anather way(easy way)
-// let result = `The blog called ${title} by ${author} has ${likes} likes`;
-// console.log(result);
-
-// creating html templates
-// let html = `
-//   <h2>${title}</h2>
-//   <p>By ${author}</p>
-//   <span>This blog has ${likes} likes</span>
-// `;
-
-// console.log(html);
-
-// arrays
-
-let ninjas = ['shaun', 'ryu', 'chun-li'];
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-
-// array length
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join(',');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-// let result = ninjas.push('ken');
-// let result = ninjas.pop();
-
-// console.log(result);
-// console.log(ninjas);
-
-
-
-// booleans & comparisons
-// console.log(true, false, 'true', 'false');
-
-// methods can return booleans
-// let email = 'luigi@thenetninja.co.uk';
-// let names = ['mario', 'luigi', 'toad'];
-
-// let result = email.includes('@');
-// let result = names.includes('luigi');
-
-// console.log(result);
-
-// comparison operators
-// let age = 25;
-
-// console.log(age == 25);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 25);
-// console.log(age >= 25);
-
-// let name = 'shaun';
-
-// console.log(name == 'shaun');
-// console.log(name == 'Shaun');
-// console.log(name > 'crystal');
-// console.log(name > 'Shaun');
-// console.log(name > 'Crystal');
-
-
-
-// type conversion
-// let score = '100';
-
-// score = Number(score);
-// console.log(score + 1);
-// console.log(typeof score);
-
-// let result = Number('hello');
-// let result = String(50);
-// let result = Boolean(100);
-// let result = Boolean(0);
-// let result = Boolean('0');
-// let result = Boolean('');
-
+// alert('hello world');
+// console.log('hello world');
+
+
+// for loops
+
+//  for(let i = 0; i < 5; i++){
+//    console.log('loop: ', i);
+// }
+// console.log('loop finished');
+// const names = ['syed', 'ishad', 'hussain'];
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i]);
+//      let html = `<div>${names[i]}</div>`;
+//      console.log(html);
+//   }
+
+  //while loops
+
+// const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+
+ // while(i < 5){
+//   console.log('loop: ', i);
+//   i++;
+// }
+
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++;
+// }
+
+// i = 8;
+// while(i > 5){
+//   console.log('loop: ', i);
+//   i--;
+// }
+
+// do while loops
+// let i = 5;
+
+// do{
+//   console.log('val of i is: ', i);
+//   i++;
+// } while(i < 5);
+
+// if statements
+// const age = 25;
+
+// if(age > 20){
+//   console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//   console.log("that's a lot of ninjas!");
+// }
+
+// const password = 'p@ssword';
+
+// if(password.length >= 8){
+//   console.log('that password is long enough!');
+// }
+
+// else if statements
+// const password = 'p@ssword123456';
+
+// if(password.length >= 12){
+//   console.log('that password is mighty strong');
+// } else if(password.length >= 8){
+//   console.log('that password is long enough');
+// } else {
+//   console.log('password should be at least 8 characters long');
+// }
+// else if statements
+// const password = 'p@ss12';
+
+// if(password.length >= 12 && password.includes('@')){
+//   console.log('that password is mighty strong');
+// } else if(password.length >= 8 || password.includes('@') && password.length > 5){
+//   console.log('that password is strong enough');
+// } else {
+//   console.log('that password is not strong enough');
+// }
+
+//  Logical NOT
+// const user = false;
+
+// if(!user){
+//   // do something
+//   console.log('you must be logged in to continue');
+// }
+
+// console.log(!true);
+// console.log(!false);
