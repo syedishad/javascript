5b5829481b96d992c1665fedb72ddf143e2047a5
SYED ISHAD HUSSAIN <82176036+syedishad@users.noreply.github.com>
2022-10-14 14:46:32 +0530

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -94,3 +94,78 @@
 
 // console.log(!true);
 // console.log(!false);
+
+// break & continue
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for(let i = 0; i < scores.length; i++){
+
+//   if(scores[i] === 0){
+//     continue;
+//   }
+
+//   console.log('your score:', scores[i]);
+
+//   if(scores[i] === 100){
+//     console.log('congrats, you got the top score!');
+//     break;
+//  
+
+// switch statements
+// const grade = 'D';
+
+// switch(grade){
+//   case 'A':
+//     console.log('you got an A!');
+//     break;
+//   case 'B':
+//     console.log('you got a B!');
+  //   break;
+  // case 'C':
+  //   console.log('you got a C!');
+  //   break;
+  // case 'D':
+  //   console.log('you got a D!');
+  //   break;
+  // case 'E':
+  //   console.log('you got an E!');
+  //   break;
+  // default:
+  //   console.log('not a valid grade');
+// }
+
+// using if statements
+// if(grade === 'A'){
+
+// } else if(grade === 'B'){
+
+// } else if(grade === 'C'){
+
+// } else if(grade === 'D'){
+
+// } else if(grade === 'E'){
+
+// } else {
+
+// }
+
+// variables & block scope
+// let age = 30;
+
+// if(true){
+
+  // age = 40;
+  // let age = 40;
+  // let name = 'shaun';
+  // console.log('inside 1st code block:', age, name);
+
+  // if(true){
+
+    // let age = 50;
+    // console.log('inside 2nd code block:', age, name);
+
+//   }
+
+// }
+
+// console.log('outside code block:', age, name);
