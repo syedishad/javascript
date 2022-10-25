4edadf04b27accbbe64711b92ad07ac257c58bc0
SYED ISHAD HUSSAIN <82176036+syedishad@users.noreply.github.com>
2022-10-25 23:07:32 +0530

    Add files via upload
    asynchronous JavaScript!!!!

--- sandbox.js
+++ sandbox.js
@@ -1,171 +1,269 @@
-// alert('hello world');
-// console.log('hello world');
+// console.log(1);
+// console.log(2);
+// setTimeout(()=>{
+//     console.log('callBack Function Fired');
+// },2000);
+// console.log(3);
+// console.log(4);
 
 
-// for loops
+//http requests..
 
-//  for(let i = 0; i < 5; i++){
-//    console.log('loop: ', i);
-// }
-// console.log('loop finished');
-// const names = ['syed', 'ishad', 'hussain'];
-// for(let i = 0; i < names.length; i++){
-//     // console.log(names[i]);
-//      let html = `<div>${names[i]}</div>`;
-//      console.log(html);
+// const request = new XMLHttpRequest();
+
+// request.addEventListener('readystatechange', () => {
+//   // console.log(request, request.readyState);
+//   if (request.readyState === 4 && request.status === 200) {
+//     // console.log(request);
+//     console.log(request.responseText);
+
+//   } else if (request.readyState === 4) {
+//     console.log('could not fetch the data');
 //   }
+// });
 
-  //while loops
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 
-// const names = ['shaun', 'mario', 'luigi'];
-// let i = 0;
+//IF THE LINK IS INCORRECT
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todoos/');
+// request.send();
 
- // while(i < 5){
-//   console.log('loop: ', i);
-//   i++;
-// }
+//  call back lesson-91
+// const getTodos = (callback) => {
 
-// while(i < names.length){
-//   console.log(names[i]);
-//   i++;
-// }
+//   const request = new XMLHttpRequest();
 
-// i = 8;
-// while(i > 5){
-//   console.log('loop: ', i);
-//   i--;
-// }
+//   request.addEventListener('readystatechange', () => {
 
-// do while loops
-// let i = 5;
+//     if(request.readyState === 4 && request.status === 200){
+//       callback(undefined, request.responseText);
+//     } else if (request.readyState === 4){
+//       callback('could not fetch the data', undefined);
+//     }
 
-// do{
-//   console.log('val of i is: ', i);
-//   i++;
-// } while(i < 5);
+//   });
+  
+//   request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+//   request.send();
 
-// if statements
-// const age = 25;
+// };
 
-// if(age > 20){
-//   console.log('you are over 20 years old');
-// }
+// console.log(1);
+// console.log(2);
 
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 3){
-//   console.log("that's a lot of ninjas!");
-// }
-
-// const password = 'p@ssword';
-
-// if(password.length >= 8){
-//   console.log('that password is long enough!');
-// }
-
-// else if statements
-// const password = 'p@ssword123456';
-
-// if(password.length >= 12){
-//   console.log('that password is mighty strong');
-// } else if(password.length >= 8){
-//   console.log('that password is long enough');
-// } else {
-//   console.log('password should be at least 8 characters long');
-// }
-// else if statements
-// const password = 'p@ss12';
-
-// if(password.length >= 12 && password.includes('@')){
-//   console.log('that password is mighty strong');
-// } else if(password.length >= 8 || password.includes('@') && password.length > 5){
-//   console.log('that password is strong enough');
-// } else {
-//   console.log('that password is not strong enough');
-// }
-
-//  Logical NOT
-// const user = false;
-
-// if(!user){
-//   // do something
-//   console.log('you must be logged in to continue');
-// }
-
-// console.log(!true);
-// console.log(!false);
-
-// break & continue
-// const scores = [50, 25, 0, 30, 100, 20, 10];
-
-// for(let i = 0; i < scores.length; i++){
-
-//   if(scores[i] === 0){
-//     continue;
+// getTodos((err, data) => {
+//   console.log('callback function fired');
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(data);
 //   }
+// });
 
-//   console.log('your score:', scores[i]);
+// console.log(3);
+// console.log(4);
 
-//   if(scores[i] === 100){
-//     console.log('congrats, you got the top score!');
-//     break;
-//  
+// lesson-92
 
-// switch statements
-// const grade = 'D';
+// const getTodos = (callback) => {
 
-// switch(grade){
-//   case 'A':
-//     console.log('you got an A!');
-//     break;
-//   case 'B':
-//     console.log('you got a B!');
-  //   break;
-  // case 'C':
-  //   console.log('you got a C!');
-  //   break;
-  // case 'D':
-  //   console.log('you got a D!');
-  //   break;
-  // case 'E':
-  //   console.log('you got an E!');
-  //   break;
-  // default:
-  //   console.log('not a valid grade');
-// }
+//   const request = new XMLHttpRequest();
 
-// using if statements
-// if(grade === 'A'){
+//   request.addEventListener('readystatechange', () => {
 
-// } else if(grade === 'B'){
+//     if(request.readyState === 4 && request.status === 200){
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4){
+//       callback('could not fetch the data', undefined);
+//     }
 
-// } else if(grade === 'C'){
+//   });
+  
+//   request.open('GET', 'todos.json');
+//   request.send();
 
-// } else if(grade === 'D'){
+// };
 
-// } else if(grade === 'E'){
-
-// } else {
-
-// }
-
-// variables & block scope
-// let age = 30;
-
-// if(true){
-
-  // age = 40;
-  // let age = 40;
-  // let name = 'shaun';
-  // console.log('inside 1st code block:', age, name);
-
-  // if(true){
-
-    // let age = 50;
-    // console.log('inside 2nd code block:', age, name);
-
+// getTodos((err, data) => {
+//   console.log('callback function fired');
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(data);
 //   }
+// });
 
-// }
 
-// console.log('outside code block:', age, name);
+//lesson-93
+// const getTodos = (resource, callback) => {
+
+//   const request = new XMLHttpRequest();
+
+//   request.addEventListener('readystatechange', () => {
+
+//     if(request.readyState === 4 && request.status === 200){
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4){
+//       callback('could not fetch the data', undefined);
+//     }
+
+//   });
+  
+//   request.open('GET', resource);
+//   request.send();
+
+// };
+
+// getTodos('todos/luigs.json', (err, data) => {
+//   console.log(data);
+//   getTodos('todos/mario.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/shaun.json', (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+//lesson-94
+// const getTodos = (resource) => {
+
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', () => {
+  
+//       if(request.readyState === 4 && request.status === 200){
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4){
+//         reject('could not fetch the data');
+//       }
+  
+//     });
+    
+//     request.open('GET', resource);
+//     request.send();
+//   });
+
+// };
+
+// getTodos('todos/luigs.json').then(data => {
+//   console.log('promise resolved:', data);
+// }).catch(err => {
+//   console.log('promise rejected:', err);
+// });
+
+// // promise example
+// const getSomething = () => {
+
+//   return new Promise((resolve, reject) => {
+//     // do something (fetch data)
+//     // resolve('some data');
+//     reject('some error');
+//   });
+
+// };
+// getSomething().then(data => {
+//   console.log('promise resolved:', data);
+// }, err => {
+//   console.log('promise rejected:', err);
+// });
+
+// getSomething().then(data => {
+//   console.log('promise resolved:', data);
+// }).catch(err => {
+//   console.log('promise rejected:', err);
+// });
+
+
+//lesson-95
+
+// const getTodos = (resource) => {
+
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', () => {
+  
+//       if(request.readyState === 4 && request.status === 200){
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4){
+//         reject('could not fetch the data');
+//       }
+  
+//     });
+    
+//     request.open('GET', resource);
+//     request.send();
+//   });
+
+// };
+// getTodos('todos/luigs.json').then(data => {
+//   console.log('promise 1 resolved:', data);
+//   return getTodos('todos/mario.json');
+// }).then(data => {
+//   console.log('promise 2 resolved:', data);
+//   return getTodos('json/shaun.json');
+// }).then(data => {
+//   console.log('promise 3 resolved:', data);
+// }).catch(err => {
+//   console.log('promise rejected:', err);
+// });
+
+// lesson-96   fetch api
+
+
+// fetch('todos/luigs.json').then(response => {
+//   //console.log(response);
+//   return response.json();
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+//lesson-97
+
+// asynn and Wait
+
+// const getTodos = async () => {
+
+//   let response = await fetch('todos/luigs.json');
+//   let data = await response.json();
+//   return data;
+
+// };
+
+// console.log(1);
+// console.log(2);
+
+// getTodos()
+//   .then(data => console.log('resolved:', data));
+
+// console.log(3);
+// console.log(4);
+
+// // console.log(getTodos());
+
+//lesson-98 throwing and catching errors
+
+
+const getTodos = async () => {
+
+  let response = await fetch('todos/luigs.json');
+
+  if(response.status !== 200){
+    throw new Error('cannot fetch the data');
+  }
+
+  let data = await response.json();
+  return data;
+
+};
+
+getTodos()
+  .then(data => console.log('resolved:', data))
+  .catch(err => console.log('rejected:', err.message));
